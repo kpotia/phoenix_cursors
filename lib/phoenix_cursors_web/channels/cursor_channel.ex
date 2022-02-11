02722ed81441ce65f5b31ea38b6742ffd268052c
Mr Kpoti <patrickamtepe@gmail.com>
2022-02-11 10:49:27 +0000

    End of tutorial

---
+++ lib/phoenix_cursors_web/channels/cursor_channel.ex
@@ -0,0 +1,62 @@
+defmodule PhoenixCursorsWeb.CursorChannel do
+  require Logger
+  alias PhoenixCursorsWeb.Presence
+  use PhoenixCursorsWeb, :channel
+
+
+
+  @impl true
+  def join("cursor:lobby", _payload, socket) do
+    send(self(), :after_join)
+    {:ok, socket}
+  end
+
+  @impl true
+  def handle_info(:after_join, socket) do
+    {:ok, _} =
+      Presence.track(socket, socket.assigns.current_user, %{
+        online_at: inspect(System.system_time(:second)),
+        color: PhoenixCursors.Colors.getHSL(socket.assigns.current_user)
+      })
+
+    push(socket, "presence_state", Presence.list(socket))
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_in("move", %{"x" => x, "y" => y}, socket) do
+    {:ok, _} =
+      Presence.update(socket, socket.assigns.current_user, fn previousState ->
+        Map.merge(
+          previousState,
+          %{
+            online_at: inspect(System.system_time(:second)),
+            x: x,
+            y: y,
+            color: PhoenixCursors.Colors.getHSL(socket.assigns.current_user)
+          }
+        )
+      end)
+
+    {:noreply, socket}
+  end
+  # Channels can be used in a request/response fashion
+  # by sending replies to requests from the client
+  @impl true
+  def handle_in("ping", payload, socket) do
+    {:reply, {:ok, payload}, socket}
+  end
+
+  # It is also common to receive messages from the client and
+  # broadcast to everyone in the current topic (cursor:lobby).
+  @impl true
+  def handle_in("shout", payload, socket) do
+    broadcast(socket, "shout", payload)
+    {:noreply, socket}
+  end
+
+  # Add authorization logic here as required.
+  defp authorized?(_payload) do
+    true
+  end
+end
